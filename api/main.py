e070cb93e82dfa043e5759d3b8db9ba6df5081f3
Juan Carlos Muñoz Ramos <juankimr@gmail.com>
2022-07-30 22:42:59 +0200

    ADD cars testcases

--- api/main.py
+++ api/main.py
@@ -1,8 +1,40 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.requests import Request
+from starlette.responses import Response
 
 app = FastAPI()
 
 
+class ContentTypeValidator:
+    TYPES = {'json': 'application/json'}
+
+    def __init__(self, request: Request):
+        self.content_type = request.headers.get('content-type')
+
+    def validate(self, expected):
+        if self.content_type == self.TYPES[expected]:
+            return True
+        return HTTPStatus.BAD_REQUEST
+
+
+class Car(BaseModel):
+    id: int
+    seats: int
+
+
 @app.get("/status")
 async def status():
+    return {"status": "OK"}
+
+
+@app.put('/cars')
+async def load_cars(request: Request = None):
+    content_type_validation = ContentTypeValidator(request).validate('json')
+    if ContentTypeValidator(request).validate('json') != True:
+        return Response(status_code=content_type_validation)
+
+    cars = await request.json()
     return {"status": "OK"}
--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 from http import HTTPStatus
 from fastapi.testclient import TestClient
@@ -16,4 +17,35 @@ class TestApiResponses(unittest.TestCase):
         Responses:
         200 OK When the service is ready to receive requests."""
         response = client.get('/status')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(HTTPStatus.OK, response.status_code)
+
+    def test_put_cars(self):
+        """ PUT /cars
+        Load the list of available cars in the service and remove all previous data
+        (existing journeys and cars).
+        This method may be called more than once during the life cycle of the service.
+        Body required The list of cars to load.
+        Content Type application/json
+
+        Responses:
+
+        200 OK When the list is registered correctly.
+        400 Bad Request When there is a failure in the request format, expected headers,
+         or the payload can't be unmarshalled.
+        """
+        with open('data_provider/cars.json') as file:
+            cars = json.load(file)
+        response = client.put('/cars',
+                              headers={'Content-Type': 'application/json'},
+                              json=cars)
+
+        self.assertEqual(HTTPStatus.OK, response.status_code)
+
+    def test_put_cars_without_content_type(self):
+        with open('data_provider/cars.json') as file:
+            cars = json.load(file)
+        response = client.put('/cars',
+                              headers={'Content-Type': ''},
+                              json=cars)
+
+        self.assertEqual(HTTPStatus.BAD_REQUEST, response.status_code)
